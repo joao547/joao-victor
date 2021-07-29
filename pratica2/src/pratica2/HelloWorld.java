04c3cc22cb4c3524a1e700cae6d1d883c7d251b5
joao547 <joaobezerra547@gmail.com>
2021-07-29 20:31:38 -0300

    Commit na hotfix

--- pratica2/src/pratica2/HelloWorld.java
+++ pratica2/src/pratica2/HelloWorld.java
@@ -3,7 +3,7 @@ package pratica2;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hellor world!");
+		System.out.println("Hellor world! hotfix");
 	}
 
 }
